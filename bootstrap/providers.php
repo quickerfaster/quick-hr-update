961bc0d532535e32556698970335b29e3fa7d4cb
Aminu A Mohammed <honestee2013@gmail.com>
2026-09-07 15:52:10 +0100

    fix: register OrganizationServiceProvider in bootstrap/providers.php

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,6 +4,7 @@ use App\Modules\Attendance\Providers\AttendanceServiceProvider;
 use App\Modules\Holiday\Providers\HolidayServiceProvider;
 use App\Modules\Hr\Providers\HrsServiceProvider;
 use App\Modules\Leave\Providers\LeaveServiceProvider;
+use App\Modules\Organization\Providers\OrganizationServiceProvider;
 use App\Modules\Payroll\Providers\PayrollServiceProvider;
 use App\Providers\AppServiceProvider;
 use App\Providers\FortifyServiceProvider;
@@ -15,5 +16,6 @@ return [
     HolidayServiceProvider::class,
     HrsServiceProvider::class,
     LeaveServiceProvider::class,
+    OrganizationServiceProvider::class,
     PayrollServiceProvider::class,
 ];
